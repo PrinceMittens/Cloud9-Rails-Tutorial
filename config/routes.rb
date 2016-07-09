882148f359559c32cb649c35a2b4b8e9d7bc4e77
PrinceMittens <prmittens@gmail.com>
2016-07-09 03:35:07 +0000

    Add hello to controller then called from route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-=======
-  root 'application#goodbye'
->>>>>>> 96c7188940e463d76050e997acef414d85157067
+  root 'application#hello'
 end
